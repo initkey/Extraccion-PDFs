8b831ef23e7c639a49250f84de66c1a2776253e4
Initkey <jvelazquez.kb@gmail.com>
2025-01-12 11:32:04 -0600

    Se agregue función get_connection_date para colocar la fecha en la que se logueo el usuario en la vista main_view

--- models/session_model.py
+++ models/session_model.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 class SessionModel:
     
     #Creamos el constructor
     def __init__(self):
         self.user_name = None
     
+    def get_connection_date(self):
+        return datetime.now().strftime(f"%d/%m/%Y %I:%M:%S %p")
+
     #Guardamos el nombre del usuario
     def set_user_name(self,name):
         self.user_name = name
