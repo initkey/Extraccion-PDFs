9b432149752f312ec1d1282d794d2d5f04d45c96
Initkey <jvelazquez.kb@gmail.com>
2025-01-11 21:57:53 -0600

    Se agrega nuevamente la función center_on_screen y se manda a llamar para centrar la aplicación en la pantalla, dado que, no logre encontrar como recuperar el commit db2e03e

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flet as ft
+from pyautogui import size
 
 def main(page:ft.Page):
 
@@ -10,6 +11,13 @@ def main(page:ft.Page):
     page.window.maximizable = False
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
+    def center_on_screen():
+        # Obtener las dimensiones de la pantalla con la librería pyautogui
+        screen_width, screen_height = size()
+        # Centramos la aplicación en la pantalla
+        page.window.left = (screen_width - page.window.width) // 2
+        page.window.top = (screen_height - page.window.height) // 2
+    center_on_screen()
     page.add(ft.Text("Primera vista"))
 
 ft.app(target=main)
