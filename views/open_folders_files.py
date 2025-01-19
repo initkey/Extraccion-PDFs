f3ceda6bb8e47280ffc504c84a169485162ab83c
Initkey <jvelazquez.kb@gmail.com>
2025-01-19 11:11:18 -0600

    Se crea el archivo con la clase OpenFoldersFiles, se agrega el constructor y open_file_picker para mandarlo a llamar desde la función build

---
+++ views/open_folders_files.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+class OpenFoldersFiles():
+
+    #Definimos el constructor
+    def __init__(self,page):
+        self.page = page
+        self.pick_files_dialog = ft.FilePicker()
+        self.page.overlay.append(self.pick_files_dialog)
+
+    def open_file_picker(self,e):
+        self.pick_files_dialog.pick_files(allow_multiple=True)
+        self.page.update()
+    
+    def build(self,e):
+        return self.open_file_picker(e)
