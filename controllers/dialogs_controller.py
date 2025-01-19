c26cc277e706b9faeb0e9f7a28d4c528cc9daad4
Initkey <jvelazquez.kb@gmail.com>
2025-01-19 11:16:58 -0600

    Se agrega a la clase DialogsController la instacia de OpenFolrdersFiles, se crea la función on_click_file_picker para llamar a la vista de la instancia

--- controllers/dialogs_controller.py
+++ controllers/dialogs_controller.py
@@ -1,11 +1,17 @@
 from views.dialogs_view import DialogsView
+from controllers.open_folders_files_controller import OpenFoldersFiles
 
 class DialogsController:
 
     #Definimos el constructor
     def __init__(self,page):
         self.page = page
-        self.view = DialogsView(self.page)
+        self.view = DialogsView(self.page,self.on_click_file_picker)
+        self.view_file_picker = OpenFoldersFiles(self.page)
+
+    def on_click_file_picker(self,e):
+        self.view_file_picker.build(e)
+        self.view.close_dialog(e)
 
     def build(self,e):
         return self.view.show_dialog(e)
