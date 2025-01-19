29439356921bb61d49d3bd80ea0cb7f72162b92d
Initkey <jvelazquez.kb@gmail.com>
2025-01-19 12:18:59 -0600

    Se agrega instancia de DataModel y se pasa como parámetro a la instancia de DialogController

--- controllers/panel_buttons_controller.py
+++ controllers/panel_buttons_controller.py
@@ -1,13 +1,15 @@
 from views.panels.panel_buttons_view import PanelButtonsView
 from controllers.dialogs_controller import DialogsController
+from models.data_model import DataModel
 
 class PanelButtonsController:
     
     #Definimos el constructor
     def __init__(self,page):
         self.page = page
+        self.data = DataModel()
         self.view = PanelButtonsView()
-        self.view_dialog = DialogsController(self.page)
+        self.view_dialog = DialogsController(self.page,self.data)
         self.view.button_load.on_click = self.on_click_load
 
     def on_click_load(self,e):
