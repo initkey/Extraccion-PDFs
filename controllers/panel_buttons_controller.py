0f9e2b653a0e1fc9140f0a93cc6cc4ce0cddf2c4
Initkey <jvelazquez.kb@gmail.com>
2025-01-18 22:41:59 -0600

    Se importa el DialogsController, se agrega el on_click al boton Cargar documentos, y la función on_click_load para mandar a traer el cuadro de diálogo

--- controllers/panel_buttons_controller.py
+++ controllers/panel_buttons_controller.py
@@ -1,10 +1,17 @@
 from views.panels.panel_buttons_view import PanelButtonsView
+from controllers.dialogs_controller import DialogsController
 
 class PanelButtonsController:
     
     #Definimos el constructor
-    def __init__(self):
+    def __init__(self,page):
+        self.page = page
         self.view = PanelButtonsView()
+        self.view_dialog = DialogsController(self.page)
+        self.view.button_load.on_click = self.on_click_load
+
+    def on_click_load(self,e):
+        self.view_dialog.build(e)
 
     def build(self):
         return self.view.build()
