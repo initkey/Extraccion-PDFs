fcdb9aa1cb3c150fef8999b2a2165f1d9c7bb84b
Initkey <jvelazquez.kb@gmail.com>
2025-01-19 12:26:49 -0600

    Se agregar el data que es la instancia de DataModel, se pasa a OpenFoldersFilesController y se llamada la función get_documents para recuperar los documentos seleccionados

--- controllers/dialogs_controller.py
+++ controllers/dialogs_controller.py
@@ -1,16 +1,17 @@
 from views.dialogs_view import DialogsView
-from controllers.open_folders_files_controller import OpenFoldersFiles
+from controllers.open_folders_files_controller import OpenFoldersFilesController
 
 class DialogsController:
 
     #Definimos el constructor
-    def __init__(self,page):
+    def __init__(self,page,data):
         self.page = page
         self.view = DialogsView(self.page,self.on_click_file_picker)
-        self.view_file_picker = OpenFoldersFiles(self.page)
+        self.view_file_picker = OpenFoldersFilesController(self.page,data)
 
     def on_click_file_picker(self,e):
         self.view_file_picker.build(e)
+        self.view_file_picker.get_documents()
         self.view.close_dialog(e)
 
     def build(self,e):
