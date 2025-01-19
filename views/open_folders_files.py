293af37f617bb3b46c1ab2cd0a53873d4ad13f06
Initkey <jvelazquez.kb@gmail.com>
2025-01-19 12:21:08 -0600

    Se inicializa el parámetro on_result en el pick_files_dialog

--- views/open_folders_files.py
+++ views/open_folders_files.py
@@ -5,7 +5,7 @@ class OpenFoldersFiles():
     #Definimos el constructor
     def __init__(self,page):
         self.page = page
-        self.pick_files_dialog = ft.FilePicker()
+        self.pick_files_dialog = ft.FilePicker(on_result=None)
         self.page.overlay.append(self.pick_files_dialog)
 
     def open_file_picker(self,e):
