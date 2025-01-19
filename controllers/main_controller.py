f51378ce3df15ba3a8107de4a86c1fbb72970e10
Initkey <jvelazquez.kb@gmail.com>
2025-01-19 15:59:18 -0600

    Se agrega instancia de DataModel() para poner obtener correctamente la información y se agrega al MainView

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,11 +1,13 @@
 from views.main_view import MainView
+from models.data_model import DataModel
 
 class MainController:
 
     #Definimos el constructor
     def __init__(self,session,page):
         self.page = page
-        self.view = MainView(self.page)
+        self.data = DataModel()
+        self.view = MainView(self.page,self.data)
         self.session = session
 
     def build(self):
