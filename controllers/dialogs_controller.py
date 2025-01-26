d0eaeffbca935464375585dc3cd09babd9e78fc1
Initkey <jvelazquez.kb@gmail.com>
2025-01-26 08:29:06 -0600

    pasamos como parametro la instancia de PanelSelection y llamamos a las funciones para actualizar el gridview

--- controllers/dialogs_controller.py
+++ controllers/dialogs_controller.py
@@ -4,15 +4,18 @@ from controllers.open_folders_files_controller import OpenFoldersFilesController
 class DialogsController:
 
     #Definimos el constructor
-    def __init__(self,page,data):
+    def __init__(self,page,data,selection):
         self.page = page
+        self.selection = selection
         self.view = DialogsView(self.page,self.on_click_file_picker)
         self.view_file_picker = OpenFoldersFilesController(self.page,data)
 
-    def on_click_file_picker(self,e):
+    async def on_click_file_picker(self,e):
         self.view_file_picker.build(e)
-        self.view_file_picker.get_documents()
+        await self.view_file_picker.get_documents()
         self.view.close_dialog(e)
+        await self.selection.load_data()
+        
 
     def build(self,e):
         return self.view.show_dialog(e)
