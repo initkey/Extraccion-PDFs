04661c306b5f4c1739b9e5759e657b083f2fb5c5
Initkey <jvelazquez.kb@gmail.com>
2025-01-26 14:03:20 -0600

    Se agregan las funciones para hacer uso del boton encargado de seleccionar y des-seleccionar todos los elementos dentro del grid, asi como actualizar el label del bóton

--- controllers/panel_selection_controller.py
+++ controllers/panel_selection_controller.py
@@ -1,4 +1,5 @@
 from views.panels.panel_selection_view import PanelSelectionView
+from flet import Chip
 
 class PanelSelectionController:
 
@@ -7,6 +8,28 @@ class PanelSelectionController:
         self.page = page
         self.data = data
         self.view = PanelSelectionView(page,data)
+        self.view.button_select.on_click = self.on_click_selected
+        self.button_text = self.view.button_select.content.controls[1].content
+    
+    def update_text_button(self):
+        if self.button_text.value == "Select All":
+            self.button_text.value = "Unselect All"
+        else:
+            self.button_text.value = "Select All"
+        self.page.update()
+
+    def on_click_selected(self,e):
+        if not len(self.view.grid_selection.controls) == 0:
+            for container in self.view.grid_selection.controls:
+                chip = container.content
+                if isinstance(chip,Chip):
+                    if self.button_text.value == "Select All":
+                        chip.selected = True
+                    else:
+                        chip.selected = False
+            self.update_text_button()
+            self.view.grid_selection.update()
+            
     
     async def load_data(self):
         await self.view.refresh_grid()
