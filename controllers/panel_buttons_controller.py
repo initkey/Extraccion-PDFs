cf08a9cef2fc722e992bddb4d7c240c0c8b4fa46
Initkey <jvelazquez.kb@gmail.com>
2025-01-26 08:15:11 -0600

    Se pasa el parametro de la instancia de PanelSelection y se manda al DialogController

--- controllers/panel_buttons_controller.py
+++ controllers/panel_buttons_controller.py
@@ -4,15 +4,17 @@ from controllers.dialogs_controller import DialogsController
 class PanelButtonsController:
     
     #Definimos el constructor
-    def __init__(self,page,data):
+    def __init__(self,page,data,selection):
         self.page = page
         self.data = data
+        self.selection = selection
         self.view = PanelButtonsView()
-        self.view_dialog = DialogsController(self.page,self.data)
+        self.view_dialog = DialogsController(self.page,self.data,selection)
         self.view.button_load.on_click = self.on_click_load
 
     def on_click_load(self,e):
         self.view_dialog.build(e)
+        # self.selection.load_data()
 
     def build(self):
         return self.view.build()
