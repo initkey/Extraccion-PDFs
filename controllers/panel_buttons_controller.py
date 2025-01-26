b997166f14a66f6b931c21e9fcdf5916d195ea7b
Initkey <jvelazquez.kb@gmail.com>
2025-01-26 14:45:29 -0600

    Se agrega el evento al bóton extraer para mostrar de forma temporal la ruta y los archivos recuperados para su posterior uso y extracción de información

--- controllers/panel_buttons_controller.py
+++ controllers/panel_buttons_controller.py
@@ -11,6 +11,14 @@ class PanelButtonsController:
         self.view = PanelButtonsView()
         self.view_dialog = DialogsController(self.page,self.data,selection)
         self.view.button_load.on_click = self.on_click_load
+        self.view.button_extract.on_click = self.on_click_extract
+
+    def on_click_extract(self,e):
+        container = [doc for doc in self.selection.view.grid_selection.controls]
+        self.data.set_documents_selected([doc.content.label.value for doc in container])
+        print(f"Ruta: {self.data.get_documents_path()}")
+        for document in self.data.get_documents_selected():
+            print(document)
 
     def on_click_load(self,e):
         self.view_dialog.build(e)
