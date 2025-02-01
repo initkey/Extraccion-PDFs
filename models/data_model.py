fbf271c06ba16faa254281dd3b1d4f3def93dcfc
Initkey <jvelazquez.kb@gmail.com>
2025-02-01 16:29:43 -0600

    Se crean las funciones encargadas de recuperar la información final, aún se puede mejorar ese resultado agregando otra función posteriormente

--- models/data_model.py
+++ models/data_model.py
@@ -11,6 +11,60 @@ class DataModel:
         self.documents_path = ""
         self.names = self.extract_info("config.txt")
 
+    def get_match(self,tables,data_result,initial,info_extra):
+        for data in tables:
+            pattern_match = self.check_pattern(3,data)
+            if pattern_match:
+                size = len(data)
+                info = [data[index] for index in range(initial,size)]
+                for row in info:
+                    empty_count = sum(1 for item in row if item == "" or item is None)
+                    empty_percentage = (empty_count/len(row)) * 100
+                    threshold = 70
+                    if empty_percentage < 70:
+                        for index,info in enumerate(info_extra):
+                            row.insert(index,info)
+                        #! Anexar código para mejorar la información recuperada.
+                        data_result.append(row)
+
+    def get_real_data(self,tables_doc):
+        data_result = []
+        owner = ""
+        date = ""
+        while True:
+            document = list()
+            try:
+                for _ in range(3):
+                    document.append(next(tables_doc))
+                name_document = document[0]
+                tables = document[1]
+                text_doc = document[2]
+                print(name_document)
+                if tables:
+                    owner = self.check_pattern(5,text_doc).group(1).strip() if self.check_pattern(5,text_doc) else "No encontrado"
+                    date = self.check_pattern(6,text_doc).group(1).strip() if self.check_pattern(6,text_doc) else "Sin fecha"
+                    info_extra = [name_document,owner,date]
+                    try:
+                        pattern_match_page = self.check_pattern(1,tables)
+                        pattern_owner_match = self.check_pattern(2,text_doc)
+                        if pattern_match_page:
+                            self.get_match(tables,data_result,3,info_extra)
+                        elif pattern_owner_match:
+                            self.get_match(tables,data_result,2,info_extra)
+                        else:
+                            pattern_word_match = self.check_pattern(4,tables)
+                            if not (pattern_word_match and tables):
+                                for row in tables:
+                                    row.insert(0,name_document)
+                                    row.insert(1,owner)
+                                    row.insert(2,date)
+                                data_result.append(row)
+                    except Exception:
+                        continue
+            except StopIteration:
+                break
+        return data_result
+
     def check_pattern(self,value, data):
         table_pattern = {
             1 : r'Pág\. 1\/\d{1,2}|Pág\. 1\/',
@@ -45,16 +99,9 @@ class DataModel:
         try:
             for _ in range(3):
                     document.append(next(data))
-                    print(f"Data:{document}")
             name_document = document[0]
             tables = document[1]
             text = document[2]
-            print("Nombres:")
-            print(name_document)
-            print("Tablas")
-            print(tables)
-            print("Textos:")
-            print(text)
         except Exception as e:
             print(f"Error inesperado: {e}")
         return name_document,tables,text
