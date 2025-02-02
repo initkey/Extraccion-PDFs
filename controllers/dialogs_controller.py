29f171279f29b07dc81423732bff37094326f1aa
Initkey <jvelazquez.kb@gmail.com>
2025-02-01 20:41:13 -0600

    Se agrega el pase de la vista de los botones y se habilita el button_extract y deshabilita el button_load

--- controllers/dialogs_controller.py
+++ controllers/dialogs_controller.py
@@ -4,8 +4,9 @@ from controllers.open_folders_files_controller import OpenFoldersFilesController
 class DialogsController:
 
     #Definimos el constructor
-    def __init__(self,page,data,selection):
+    def __init__(self,page,data,selection,buttons):
         self.page = page
+        self.buttons = buttons
         self.selection = selection
         self.view = DialogsView(self.page,self.on_click_file_picker)
         self.view_file_picker = OpenFoldersFilesController(self.page,data)
@@ -15,7 +16,9 @@ class DialogsController:
         await self.view_file_picker.get_documents()
         self.view.close_dialog(e)
         await self.selection.load_data()
-        
+        self.buttons.button_load.disabled = True
+        self.buttons.button_extract.disabled = False
+        self.page.update()
 
     def build(self,e):
         return self.view.show_dialog(e)
