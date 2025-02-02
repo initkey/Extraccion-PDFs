91c127e531ec64a295b0b4349c01633e907307ab
Initkey <jvelazquez.kb@gmail.com>
2025-02-01 22:05:34 -0600

    Se modifica código para visualizar solo el TextField, además de modificar el diseño visual

--- views/welcome_view.py
+++ views/welcome_view.py
@@ -4,14 +4,25 @@ class WelcomeView:
 
     #Definimos el constructor
     def __init__(self):
-        self.name_input = ft.TextField(label="Escribe tu nombre",text_align=ft.TextAlign.CENTER)
-        self.enter_button = ft.FilledButton(text="Entrar")
+        self.name_input = self.create_text_field("Bienvenida!","Escribe tu nombre")
+
+    def create_text_field(self,label,text):
+        return ft.TextField(
+                label=label,
+                label_style=ft.TextStyle(size=20,weight="Bold"),
+                hint_text=text,
+                hint_style=ft.TextStyle(size=20, weight="Normal"),
+                text_align=ft.TextAlign.CENTER,
+                text_size=25,
+                text_style=ft.TextStyle(size=30,weight="Bold"),
+                border=ft.InputBorder.UNDERLINE,
+                filled=True,
+            )
 
     def build(self):
         return ft.Column(
             [
-                self.name_input,
-                self.enter_button
+                self.name_input
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER
