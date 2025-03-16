182698d5d2b7e9fe2f6f282bf7625c704d4e2ed7
Jonathan Velazquez <jvelazquez.kb@gmail.com>
2025-03-15 22:16:23 -0600

    Se modifica el código para usar un json para guardar el nombre del usuario

--- models/session_model.py
+++ models/session_model.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 class SessionModel:
     
     #Creamos el constructor
@@ -7,11 +8,23 @@ class SessionModel:
     
     def get_connection_date(self):
         return datetime.now().strftime(f"%d/%m/%Y %I:%M:%S %p")
+    
+    # Definición de la función que modifica la palabra y le da formato
+    def format_name(self,name):
+        name = name.strip().lower()
+        return name.title()
 
-    #Guardamos el nombre del usuario
-    def set_user_name(self,name):
-        self.user_name = name
+    # Definición de la función que guarda el valor en un json
+    def set_name(self,name):
+        data = {"name": name}
+        with open("username.json","w") as file:
+            json.dump(data,file)  
 
-    #Devolvemos el nombre del usuario
-    def get_user_name(self):
-        return self.user_name
+    # Definición de la función que recupera el valor de un json
+    def get_name(self):
+        try:
+            with open("username.json","r") as file:
+                data = json.load(file)
+                return data.get("name",None)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return None
