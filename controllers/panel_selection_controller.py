8ece29bf0d4d291dc4effb897b7e638c6dccd87c
Jonathan Velazquez <jvelazquez.kb@gmail.com>
2025-03-17 21:41:57 -0600

    se crea funcion get_chip_selected para corregir error de extracción de información cuando solo se seleccionan algunos documentos

--- controllers/panel_selection_controller.py
+++ controllers/panel_selection_controller.py
@@ -1,6 +1,13 @@
 from views.panels.panel_selection_view import PanelSelectionView
 from flet import Chip
 
+"""
+
+    Corregir las funciones get_chip_selected y on_click_selected
+    Se repite el código y es necesario corregirlo cuando tenga más tiempo
+
+"""
+
 class PanelSelectionController:
 
     #Definimos el constructor
@@ -10,6 +17,19 @@ class PanelSelectionController:
         self.view = PanelSelectionView(page,data)
         self.view.button_select.on_click = self.on_click_selected
         self.button_text = self.view.button_select.content.controls[1].content
+
+    def get_chip_selected(self):
+        total_selected = 0
+        document_selected = []
+        content_grid = self.view.grid_selection.controls
+        if not len(content_grid) == 0:
+            for container in content_grid:
+                chip = container.content
+                if isinstance(chip,Chip) and chip.selected:
+                    total_selected += 1
+                    document_selected.append(container.content.label.value)
+        return [total_selected,document_selected]
+        
     
     def update_text_button(self):
         if self.button_text.value == "Select All":
@@ -29,7 +49,6 @@ class PanelSelectionController:
                         chip.selected = False
             self.update_text_button()
             self.view.grid_selection.update()
-            
     
     async def load_data(self):
         self.view.button_select.disabled = False
