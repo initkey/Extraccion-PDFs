94973843f67c95d18e7243df1a1e5b50aab2a750
Jonathan Velazquez <jvelazquez.kb@gmail.com>
2025-03-29 20:10:17 -0600

    Corregimos el uso de get y set para uso en python con @property y setter

--- models/session_model.py
+++ models/session_model.py
@@ -4,27 +4,31 @@ class SessionModel:
     
     #Creamos el constructor
     def __init__(self):
-        self.user_name = None
+        self.__username = None
     
     def get_connection_date(self):
         return time.strftime(f"%d/%m/%Y %I:%M:%S %p", time.localtime())
     
     # Definición de la función que modifica la palabra y le da formato
     def format_name(self,name):
-        name = name.strip().lower()
-        return name.title()
-
-    # Definición de la función que guarda el valor en un json
-    def set_name(self,name):
-        data = {"name": name}
-        with open("username.json","w") as file:
-            json.dump(data,file)  
+        self.__username = name.strip().lower()
+        return self.__username.title()
 
     # Definición de la función que recupera el valor de un json
-    def get_name(self):
+    @property
+    def username(self):
         try:
             with open("username.json","r") as file:
                 data = json.load(file)
-                return data.get("name",None)
+                self.__username = data.get("name",None)
+                return self.__username
         except (FileNotFoundError, json.JSONDecodeError):
             return None
+
+    # Definición de la función que guarda el valor en un json
+    @username.setter
+    def username(self,name):
+        self.__username = name
+        data = {"name": self.__username}
+        with open("username.json","w") as file:
+            json.dump(data,file)
